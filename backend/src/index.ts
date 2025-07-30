72a16d6ac14e5fc35f434d5c83da0a9130ab6766
AnastasiaSB <116894101+AnastasiyaSB@users.noreply.github.com>
2025-07-30 19:27:43 +0300

    Add users validation on messages: do not allow to send messages from non-existing user

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -36,13 +36,28 @@ async function initServer() {
 
   server.use(express.json());
 
+  async function getUsers() {
+    const usersResponse = await pgClient.query("SELECT * FROM users");
+    return usersResponse.rows as User[];
+  }
+
+  async function getUser(userId: number) {
+    const usersResponse = await pgClient.query(`SELECT * FROM users WHERE user_id = ${userId}`);
+
+    if (usersResponse.rows.length > 0) {
+      return usersResponse.rows[0] as User;
+    }
+
+    return null;
+  }
+
   server.get("/", function (req: Request, res: Response) {
     res.status(200).json("Hello from backend");
   });
 
   server.get("/users", async function (req: Request, res: Response) {
-    const usersResponse = await pgClient.query("SELECT * FROM users");
-    res.status(200).send(usersResponse.rows as User[]);
+    const usersResponse = await getUsers();
+    res.status(200).send(usersResponse);
   });
 
   server.get("/messages", async function (req: Request, res: Response) {
@@ -59,45 +74,50 @@ async function initServer() {
   });
 
   server.post("/messages", async function (req: Request, res: Response) {
-    const { username, text } = req.body;
+    const { user_id, text } = req.body;
+    if (await getUser(user_id) === null) {
+      res.status(401).send({
+        message: "Incorrect username",
+      });
 
-    function validateForm(username: unknown, text: unknown) {
-     if (typeof username !== "string") {
-      return { field: "username", message: "Incorrect username (Username must be a string)" };
-    }
-
-     if (username.length < 2) {
-      return { field: "username", message: "Incorrect length of username (too short)" };
-    }
-
-     if (username.length > 50) {
-      return { field: "username", message: "Incorrect length of username (too long)" };
-    }
-
-    if (typeof text !== "string") {
-      return { field: "text", message: "Incorrect message text (Message must be a string)" };
-    }
-
-    if (text.trim().length === 0) {
-      return { field: "text", message: "Message cannot be empty" };
-    }
-
-    if (text.length < 1) {
-      return { field: "text", message: "Incorrect length of message (too short)" };
-    }
-
-    if (text.length > 500) {
-      return { field: "text", message: "Incorrect length of message (too long)" };
-    }
-
-    return;
-  }
-
-    const error = validateForm(username, text);
-    if (error) {
-      res.status(400).send({ message: error.message });
       return;
     }
+
+    function validateForm(username: unknown, text: unknown) {
+      if (typeof username !== "string") {
+        return { field: "username", message: "Incorrect username (Username must be a string)" };
+      }
+
+      if (username.length < 2) {
+        return { field: "username", message: "Incorrect length of username (too short)" };
+      }
+
+      if (username.length > 50) {
+        return { field: "username", message: "Incorrect length of username (too long)" };
+      }
+
+      if (typeof text !== "string") {
+        return { field: "text", message: "Incorrect message text (Message must be a string)" };
+      }
+
+      if (text.trim().length === 0) {
+        return { field: "text", message: "Message cannot be empty" };
+      }
+
+      if (text.length < 1) {
+        return { field: "text", message: "Incorrect length of message (too short)" };
+      }
+
+      if (text.length > 500) {
+        return { field: "text", message: "Incorrect length of message (too long)" };
+      }
+    }
+
+    // const error = validateForm(username, text);
+    // if (error) {
+    //   res.status(400).send({ message: error.message });
+    //   return;
+    // }
     
     try {
       const newMessageResponse = await pgClient.query(`INSERT INTO messages(
@@ -105,7 +125,7 @@ async function initServer() {
         user_id
       ) VALUES (
         '${text}',
-        ${1 + Math.floor(Math.random() * 3)}
+        ${user_id}
       )`);
 
       res.sendStatus(201);
