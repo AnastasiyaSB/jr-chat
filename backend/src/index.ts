b495eadb91fef9f232449c4f14c9f6f402ac2f46
AnastasiaSB <116894101+AnastasiyaSB@users.noreply.github.com>
2025-07-31 11:55:04 +0300

    Use parametrized queries to prevent SQL-injections and unescaped characters

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -33,7 +33,7 @@ async function initServer() {
   }
 
   async function getUserById(userId: number) {
-    const usersResponse = await pgClient.query(`SELECT * FROM users WHERE user_id = ${userId}`);
+    const usersResponse = await pgClient.query(`SELECT * FROM users WHERE user_id = $1::integer`, [userId]);
 
     if (usersResponse.rows.length > 0) {
       return usersResponse.rows[0] as User;
@@ -43,7 +43,7 @@ async function initServer() {
   }
 
   async function getUserByName(username: string) {
-    const usersResponse = await pgClient.query(`SELECT * FROM users WHERE username = '${username}'`);
+    const usersResponse = await pgClient.query(`SELECT * FROM users WHERE username = $1::text`, [username]);
 
     if (usersResponse.rows.length > 0) {
       return usersResponse.rows[0] as User;
@@ -75,8 +75,8 @@ async function initServer() {
     const newUserResponse = await pgClient.query(`INSERT INTO users(
       username
     ) VALUES (
-      '${username}'
-    )`);
+      $1::text
+    )`, [username]);
 
     if (newUserResponse.rowCount === 0) {
       res.sendStatus(500);
@@ -158,21 +158,10 @@ async function initServer() {
     let newMessageResponse;
     
     try {
-      console.log(`INSERT INTO messages(
-        text,
-        user_id
-      ) VALUES (
-        '${text}',
-        ${user_id}
-      )`);
-
       newMessageResponse = await pgClient.query(`INSERT INTO messages(
         text,
         user_id
-      ) VALUES (
-        '${text}',
-        ${user_id}
-      )`);
+      ) VALUES ($1::text, $2::integer)`, [text, user_id]);
 
       res.sendStatus(201);
     } catch (err) {
